4599f6ac0293111e70697bfbdbd4405f8f4dd674
alexcarron <thealexcarron@gmail.com>
2024-01-18 16:09:02 -0500

    Add host-event command

--- commands/ll_points/host-event.js
+++ commands/ll_points/host-event.js
@@ -1,15 +1,14 @@
-const Parameter = require('../../modules/commands/Paramater.js');
 const SlashCommand = require('../../modules/commands/SlashCommand.js');
-const { LLPointAccomplishments } = require('../../modules/enums.js')
 const { deferInteraction, confirmAction, getUser } = require("../../modules/functions.js");
-const ids = require(`../../databases/ids.json`)
+const { ModalBuilder } = require('discord.js');
 
 const command = new SlashCommand({
 	name: "host-event",
 	description: "Host your own custom event on your own",
 });
+command.parameters = [
+]
 command.allowsDMs = true;
-command.required_servers = [];
 command.execute = async function(interaction) {
 	await deferInteraction(interaction);
 
@@ -33,6 +32,35 @@ command.execute = async function(interaction) {
 			return
 		}
 
-		interaction.followUp()
+		const modal = new ModalBuilder()
+			.setCustomId('myModal')
+			.setTitle('My Modal');
+
+		// Create the text input components
+		const favoriteColorInput = new TextInputBuilder()
+			.setCustomId('favoriteColorInput')
+				// The label is the prompt the user sees for this input
+			.setLabel("What's your favorite color?")
+				// Short means only a single line of text
+			.setStyle(TextInputStyle.Short);
+
+		const hobbiesInput = new TextInputBuilder()
+			.setCustomId('hobbiesInput')
+			.setLabel("What's some of your favorite hobbies?")
+				// Paragraph means multiple lines of text.
+			.setStyle(TextInputStyle.Paragraph);
+
+		// An action row only holds one text input,
+		// so you need one action row per text input.
+		const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
+		const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
+
+		// Add inputs to the modal
+		modal.addComponents(firstActionRow, secondActionRow);
+
+		// Show the modal to the user
+		await interaction.showModal(modal);
+
+		interaction.followUp("Test");
 }
 module.exports = command;
