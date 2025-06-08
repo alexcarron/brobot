6031af0b9e5cd34910faf9e1fba333744552591c
alexcarron <thealexcarron@gmail.com>
2025-06-08 15:13:54 -0500

    Add option to deploy slash commands on developer setup

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const { setupEventListeners } = require('./event-listeners/event-listener-setup.js');
 const { logInfo } = require('./utilities/logging-utils.js');
 const { Events } = require('discord.js');
-const { setupCommands } = require('./bot-config/setup-commands.js');
+const { setupCommands, setupAndDeployCommands } = require('./bot-config/setup-commands.js');
 const { setupClient } = require('./bot-config/setup-client.js');
 const { onClientReady } = require('./bot-config/on-ready.js');
+
+const DEPLOY_OPTIONS = [
+	'--deploy-commands',
+	'--deployCommands',
+	'--deploy',
+	'--dc',
+	'--d',
+];
+
+const DEPLOY_ALL_OPTIONS = [
+	'--deploy-all-commands',
+	'--deployAllCommands',
+	'--deployAll',
+	'--da',
+];
 
 const startBrobot = async () => {
 	logInfo(`Using discord.js version: ${require('discord.js').version}`);
 
 	const client = await setupClient();
 	setupEventListeners(client);
-	setupCommands(client);
+
+	const commandArguments = process.argv;
+	const isDeploying = commandArguments.some(argument => DEPLOY_OPTIONS.includes(argument));
+	const isDeployingAll = commandArguments.some(argument => DEPLOY_ALL_OPTIONS.includes(argument));
+
+	if (isDeployingAll)
+		await setupAndDeployCommands({client});
+	else if (isDeploying)
+		await setupAndDeployCommands({client, skipGlobalCommands: true});
+	else
+		setupCommands(client);
 
 	// when the client is ready, run this code
 	// this event will only trigger one time after Brobot has successfully fully connected to the Discord API
