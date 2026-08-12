4e6d14e6c80742dc287d6ed6b1d174ccbf0a5459
alexcarron <thealexcarron@gmail.com>
2026-08-12 09:42:47 -0500

    Add rearrange name workflow and update /rearrange-name command file to use workflow

---
+++ services/namesmith/workflows/rearrange-name.workflow.test.ts
@@ -0,0 +1,83 @@
+jest.mock("../utilities/discord-action.utility", () => ({
+	changeDiscordNameOfPlayer: jest.fn(),
+}));
+
+import { makeSure } from "../../../utilities/jest/jest-utils";
+import { DatabaseQuerier } from "../database/database-querier";
+import { setupMockNamesmith } from "../mocks/mock-setup";
+import { getNamesmithServices } from "../services/get-namesmith-services";
+import { PlayerService } from "../services/player.service";
+import { rearrangeName } from './rearrange-name.workflow';
+import { addMockPlayer } from "../mocks/mock-data/mock-players";
+import { returnIfNotFailure } from "../utilities/workflow.utility";
+import { getLatestActivityLog } from "../mocks/mock-data/mock-activity-logs";
+import { ActivityTypes } from "../types/activity-log.types";
+
+describe('rearrange-name.workflow', () => {
+	let playerService: PlayerService;
+	let db: DatabaseQuerier;
+
+	beforeEach(() => {
+		setupMockNamesmith();
+		const services = getNamesmithServices();
+		playerService = services.playerService;
+		db = playerService.playerRepository.db;
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	afterAll(() => {
+		jest.restoreAllMocks();
+	});
+
+	describe('rearrangeName()', () => {
+		it('returns notAPlayer if the player is not a player.', () => {
+			const result = rearrangeName({player: 'not-a-player-id', newName: 'abc'});
+
+			makeSure(result.isNotAPlayer()).isTrue();
+		});
+
+		it('returns hasExtraCharacters if the new name contains characters not in the inventory.', () => {
+			const player = addMockPlayer(db, {
+				inventory: 'abc',
+				currentName: 'abc'
+			});
+
+			const result = rearrangeName({player, newName: 'abcd'});
+
+			makeSure(result.isHasExtraCharacters()).isTrue();
+		});
+
+		it('changes the player\'s current name and returns the new name and unused characters.', () => {
+			const player = addMockPlayer(db, {
+				inventory: 'abcd',
+				currentName: 'abc'
+			});
+
+			const {newName, unusedCharacters} = returnIfNotFailure(
+				rearrangeName({player, newName: 'cab'})
+			);
+
+			makeSure(newName).is('cab');
+			makeSure(unusedCharacters).is(['d']);
+			makeSure(playerService.getCurrentName(player)).is('cab');
+		});
+
+		it('creates an activity log with accurate metadata.', () => {
+			const player = addMockPlayer(db, {
+				inventory: 'abcd',
+				currentName: 'abc'
+			});
+
+			rearrangeName({player, newName: 'cab'});
+
+			const activityLog = getLatestActivityLog(db);
+			makeSure(activityLog.player.id).is(player.id);
+			makeSure(activityLog.type).is(ActivityTypes.CHANGE_NAME);
+			makeSure(activityLog.nameChangedFrom).is('abc');
+			makeSure(activityLog.currentName).is('cab');
+		});
+	});
+});
---
+++ services/namesmith/workflows/rearrange-name.workflow.ts
@@ -0,0 +1,61 @@
+import { getCharacterDifferences } from "../../../utilities/data-structure-utils";
+import { getNamesmithServices } from "../services/get-namesmith-services";
+import { PlayerResolvable } from "../types/player.types";
+import { getWorkflowResultCreator, provides } from "./workflow-result-creator";
+
+const result = getWorkflowResultCreator({
+	success: provides<{
+		newName: string,
+		unusedCharacters: string[],
+	}>(),
+
+	notAPlayer: provides<{}>(),
+	hasExtraCharacters: provides<{
+		extraCharacters: string
+	}>(),
+})
+
+/**
+ * Rearranges a player's current name into a new name.
+ * @param parameters - The parameters for the function
+ * @param parameters.player - The player rearranging their name
+ * @param parameters.newName - The new arrangement of characters to set as the player's current name
+ * @returns The new name and any characters not used in it in the player's inventory.
+ * - hasExtraCharacters if the new name contains characters the player does not have in their inventory.
+ */
+export const rearrangeName = (
+	{player: playerResolvable, newName}: {
+		player: PlayerResolvable;
+		newName: string;
+	}
+) => {
+	const {playerService, activityLogService} = getNamesmithServices();
+
+	if (!playerService.isPlayer(playerResolvable)) {
+		return result.failure.notAPlayer({});
+	}
+
+	const inventory = playerService.getInventory(playerResolvable);
+	const { extraCharacters } = getCharacterDifferences(inventory, newName);
+
+	if (extraCharacters.length > 0) {
+		return result.failure.hasExtraCharacters({
+			extraCharacters: extraCharacters.join(''),
+		});
+	}
+
+	const nameBefore = playerService.getCurrentName(playerResolvable);
+	playerService.changeCurrentName(playerResolvable, newName);
+
+	activityLogService.logChangeName({
+		playerChangingName: playerResolvable,
+		nameBefore,
+	});
+
+	const unusedCharacters = playerService.getUnusedInventoryCharacters(playerResolvable, newName);
+
+	return result.success({
+		newName,
+		unusedCharacters,
+	});
+};
